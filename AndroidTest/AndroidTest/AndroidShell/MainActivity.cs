3b9d3dc0cd4ceb8c8f7fb49f16c841180c217679
zhangfei2211 <358837621@qq.com>
2022-05-01 18:14:34 +0800

    demo modification

--- AndroidTest/AndroidTest/AndroidShell/MainActivity.cs
+++ AndroidTest/AndroidTest/AndroidShell/MainActivity.cs
@@ -17,20 +17,20 @@ namespace AndroidShell
             SetContentView(Resource.Layout.activity_main);
 
 
-            //WebView discoverWebView = FindViewById<WebView>(Resource.Id.webViewDiscover);
-            ////set the webview client
-            //discoverWebView.SetWebViewClient(new WebViewClient());
-            ////load the subscription url
-            //discoverWebView.LoadUrl("https://www.baidu.com/");
-            ////enable javascript in our webview
-            //discoverWebView.Settings.JavaScriptEnabled = true;
-            ////zoom control on?  This should perhaps be disabled for consistency?
-            ////we will leave it on for now
-            //discoverWebView.Settings.BuiltInZoomControls = true;
-            //discoverWebView.Settings.SetSupportZoom(true);
-            ////scrollbarsdisabled
-            //// subWebView.ScrollBarStyle = ScrollbarStyles.OutsideOverlay;
-            //discoverWebView.ScrollbarFadingEnabled = false;
+            WebView discoverWebView = FindViewById<WebView>(Resource.Id.webViewDiscover);
+            //set the webview client
+            discoverWebView.SetWebViewClient(new WebViewClient());
+            //load the subscription url
+            discoverWebView.LoadUrl("https://192.168.0.103:50209/Login");
+            //enable javascript in our webview
+            discoverWebView.Settings.JavaScriptEnabled = true;
+            //zoom control on?  This should perhaps be disabled for consistency?
+            //we will leave it on for now
+            discoverWebView.Settings.BuiltInZoomControls = true;
+            discoverWebView.Settings.SetSupportZoom(true);
+            //scrollbarsdisabled
+            // subWebView.ScrollBarStyle = ScrollbarStyles.OutsideOverlay;
+            discoverWebView.ScrollbarFadingEnabled = false;
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
